6b36aa9308c53bcaf807f41d1d4de5f5b3789723
QiCong <qicong113@foxmail.com>
2017-04-12 16:40:16 +0800

    parameter

---
+++ Mybatis/src/demo/h_parameter/UserExample.java
@@ -0,0 +1,20 @@
+package demo.h_parameter;
+
+public class UserExample {
+	private Double minMoney;
+	private Double maxMoney;
+	public UserExample() {
+	}
+	public Double getMinMoney() {
+		return minMoney;
+	}
+	public void setMinMoney(Double minMoney) {
+		this.minMoney = minMoney;
+	}
+	public Double getMaxMoney() {
+		return maxMoney;
+	}
+	public void setMaxMoney(Double maxMoney) {
+		this.maxMoney = maxMoney;
+	}
+}
